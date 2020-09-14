76e0edb7b96cc729e861a611d0ccf54e35651d5f
Carl-Magnus <carl.magnus@hotmail.com>
2020-09-14 15:19:31 +0200

    Added basic FPS movement functionality, as well as a subfolder for player scripts.

---
+++ Assets/Scripts/PlayerScripts/PlayerCamera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    [SerializeField] private float mouseSensitivity = 100f;
+
+    public Transform playerBody;
+
+    float xRotation = 0f;
+
+    void Start()
+    {
+        playerBody = transform.parent.gameObject.transform;
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    public Transform groundCheck;
+
+    public LayerMask groundMask;
+
+    private Vector3 velocity;
+
+    [SerializeField] private bool isGrounded;
+
+    [SerializeField] private float speed;
+    [SerializeField] private float gravity;
+    [SerializeField] private float groundDistance;
+    [SerializeField] private float jumpHeight;
+
+    void Start()
+    {
+        controller = gameObject.GetComponent<CharacterController>();
+        groundCheck = transform.Find("GroundCheck").transform;
+
+        speed = 12f;
+        gravity = -19.64f;
+        groundDistance = 0.4f;
+        jumpHeight = 3f;
+
+    }
+
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        Movement();
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            Jump();
+        }
+    }
+
+    void Movement()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move * speed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+    }
+
+    void Jump()
+    {
+        velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+    }
+}
