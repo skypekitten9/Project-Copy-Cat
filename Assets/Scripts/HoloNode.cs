3de91bc736dc55e74d6085485165846c6887b076
Filip Moreau <filip.moreau@hotmail.com>
2020-09-15 19:49:01 +0200

    Added holo nodes and record script
    Holo nodes contain necessary data for tracking the hologram's movement and actions.
    
    The record script spaws holo nodes at a constant rate
    
    Co-Authored-By: Victor Enhörning <47422880+skypekitten9@users.noreply.github.com>

---
+++ Assets/Scripts/HoloNode.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public enum Action { None, Jump, Interact}
+
+public class HoloNode
+{
+    public HoloNode(Vector3 position, float timePassed, Action action)
+    {
+        this.Position = position;
+        this.Time = timePassed;
+        this.HoloAction = action;
+    }
+
+    public Vector3 Position { get; private set; }
+    public float Time { private set; get; }
+    public Action HoloAction { private set; get; }
+}
---
+++ Assets/Scripts/Record.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+
+public class Record : MonoBehaviour
+{
+    private Stopwatch stopwatch;
+    private List<HoloNode> holoNodes;
+    private float nodeSpawnRate = 2; //times per second
+
+    public void StartRecording()
+    {
+        UnityEngine.Debug.Log("Start record");
+
+        stopwatch = new Stopwatch();
+        holoNodes = new List<HoloNode>();
+        stopwatch.Start();
+    }
+
+
+    private void Update()
+    {
+        if (stopwatch != null)
+        {
+            if (stopwatch.ElapsedMilliseconds >= (1 / nodeSpawnRate * 1000) * holoNodes.Count)
+            {
+                holoNodes.Add(new HoloNode(Vector3.zero, stopwatch.ElapsedMilliseconds, Action.None));
+            }
+        }
+
+
+
+        /*STUB*/
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            StartRecording();
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            EndRecording();
+        }
+
+    }
+
+    public void EndRecording()
+    {
+        UnityEngine.Debug.Log("End record");
+        if (stopwatch != null)
+        {
+            stopwatch.Stop();
+            stopwatch = null;
+        }
+    }
+
+
+    /// <summary>
+    /// Debug to visualize holo nodes
+    /// </summary>
+    private void OnDrawGizmos()
+    {
+        if (holoNodes != null)
+        {
+            foreach (var node in holoNodes)
+            {
+                Gizmos.color = Color.red;
+                Gizmos.DrawWireSphere(node.Position, 0.5f);
+            }
+        }
+    }
+}
