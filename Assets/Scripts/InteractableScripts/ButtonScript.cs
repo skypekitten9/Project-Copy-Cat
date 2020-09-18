0b042ad22fa8a8817dd6fcc7ca47116cd53d2883
Carl-Magnus <carl.magnus@hotmail.com>
2020-09-18 10:59:07 +0200

    Påbörjat strukturen för hur dörrar och knappar ska interagera med varandra genom en level manager.

---
+++ Assets/Scripts/InteractableScripts/ButtonScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    //Denna metoden är tänkt man ska kalla på när spelaren klickar på knappen. Den meddelar senare vidare till LevelManagern att kanalen ska uppdateras så dörren öppnas.
+    private void SignalChannel()
+    {
+
+    }
+}
---
+++ Assets/Scripts/ManagerScripts/TestLevelManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestLevelManager : MonoBehaviour
+{
+    private static TestLevelManager instance = null;
+    public static TestLevelManager Instance { get { return instance; } }
+
+    //Listan som håller alla bools för om dörrar ska vara stängda eller öppna.
+    public List<bool> channelList;
+
+    public void Awake()
+    {
+        if (instance != null && instance != this)
+            Destroy(this.gameObject);
+        else
+            instance = this;
+    }
+
+    private void Start()
+    {
+        channelList = new List<bool>();
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    //Metoden som knapparnas interaktion ska kalla på. Den gör så att kanalens bool värde ändras för den specifika knappen, och säger åt alla dörrar att titta på sina kanaler.
+    public void UpdateChannels()
+    {
+
+    }
+
+}
