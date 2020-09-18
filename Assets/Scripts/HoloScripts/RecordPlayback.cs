7756ffedf695c3075d49b200208a56617ff1cdd7
Filip Moreau <filip.moreau@hotmail.com>
2020-09-18 23:52:46 +0200

    Added record-phases, Time accurate playback, interaction points
    Record phases:
     None - ok to start a new recording
     Recording - currentrly recording the movements and interactions
     Rewinding - bringing back the player to where the recording started
     SpawningHolo - Instantiating the holo
     PlayingBack - Moving the holo  along the recorded path & interacting
     KillingHolo - Destroying the holo instance

--- Assets/Scripts/HoloScripts/RecordPlayback.cs
+++ Assets/Scripts/HoloScripts/RecordPlayback.cs
@@ -1,90 +1,92 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using UnityEditor.UIElements;
 using UnityEngine;
+
+public enum RecordPhase { None, Recording, Rewinding, SpawningHolo, PlayingBack, KillingHolo }
 
 public class RecordPlayback : MonoBehaviour
 {
-    [SerializeField] private GameObject holo;
+    [SerializeField] private GameObject holoPrefab;
+    private GameObject holoInstance;
 
     private Stopwatch stopwatch;
-    private List<HoloNode> holoNodes;
-    private float nodeSpawnRate = 10; //times per second
+    private List<HoloNode> holoPositionNodes;
+    private List<HoloNode> holoInteractNodes;
 
-    private float recordTime = 5000; //ms
+    [SerializeField] private float nodeSpawnRate = 10;      //How often to spawn a node (times per second)
+    [SerializeField] private float recordTime = 5000;       //The timeframe for recording (milliseconds)
+    [SerializeField] private float rewindSpeed = 0.75f;     //The player's speed duing the rewind-phase
 
-    private bool recording = false;
-    private bool rewinding = false;
-    private int nodeCounter;
-
-    private float rewindSpeed = 0.75f;
-
+    private RecordPhase recordPhase = RecordPhase.None;
 
     private void Update()
     {
-        if (recording)
+        /*Stub - let InputManager call this*/
+        if (recordPhase == RecordPhase.Recording && Input.GetKeyDown(KeyCode.E))
         {
-            if (stopwatch.ElapsedMilliseconds >= recordTime)
-            {
-                StopRecording();
-                nodeCounter = holoNodes.Count - 1;
-                StartCoroutine(Rewind());
-            }
-            else if (stopwatch.ElapsedMilliseconds >= (1 / nodeSpawnRate * 1000) * holoNodes.Count)
-            {
-                holoNodes.Add(new HoloNode(PlayerManager.Instance.transform.position, PlayerManager.Instance.transform.rotation, stopwatch.ElapsedMilliseconds, Action.None));
-            }
+            holoInteractNodes.Add(new HoloNode(PlayerManager.Instance.transform.position, PlayerManager.Instance.transform.rotation, stopwatch.ElapsedMilliseconds, Action.Interact));
         }
 
-
-        /*STUB*/
+        /*Stub - let InputManager call this*/
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            if (!recording && !rewinding)
-                StartRecording();
+            if (recordPhase == RecordPhase.None)
+            {
+                StartCoroutine(Record());
+            }
         }
-
     }
 
-    public void StartRecording()
-    {
-        UnityEngine.Debug.Log("Start record");
 
+    System.Collections.IEnumerator Record()
+    {
+        UnityEngine.Debug.Log("Recording");
+        recordPhase = RecordPhase.Recording;
+
+        holoPositionNodes = new List<HoloNode>();
+        holoInteractNodes = new List<HoloNode>();
         stopwatch = new Stopwatch();
-        holoNodes = new List<HoloNode>();
-        recording = true;
         stopwatch.Start();
-    }
 
-    public void StopRecording()
-    {
-        UnityEngine.Debug.Log("End record");
-        recording = false;
-        if (stopwatch != null)
+        while (stopwatch.ElapsedMilliseconds < recordTime)
         {
-            stopwatch.Stop();
-            stopwatch = null;
-        }
-    }
+            if (stopwatch.ElapsedMilliseconds >= (1 / nodeSpawnRate * 1000) * holoPositionNodes.Count)
+            {
+                holoPositionNodes.Add(new HoloNode(PlayerManager.Instance.transform.position, PlayerManager.Instance.transform.rotation, stopwatch.ElapsedMilliseconds, Action.None));
+            }
 
+            yield return new WaitForFixedUpdate();
+        }
+
+        stopwatch.Stop();
+
+        StartCoroutine(Rewind());
+    }
 
     System.Collections.IEnumerator Rewind()
     {
-        rewinding = true;
-        float tolerance = rewindSpeed;
+        UnityEngine.Debug.Log("Rewinding");
+        recordPhase = RecordPhase.Rewinding;
 
-        PlayerManager.Instance.GetComponent<CharacterController>().enabled = false;
+
+        PlayerManager.Instance.GetComponent<PlayerMovement>().enabled = false;
         PlayerManager.Instance.GetComponentInChildren<PlayerCamera>().enabled = false;
+        PlayerManager.Instance.GetComponent<CharacterController>().enabled = false;
+
+        float tolerance = rewindSpeed;
+        int nodeCounter = holoPositionNodes.Count - 1;
 
         while (nodeCounter > 0)
         {
-            while (Vector3.Distance(PlayerManager.Instance.transform.position, holoNodes[nodeCounter - 1].Position) >= tolerance)
+            while (Vector3.Distance(PlayerManager.Instance.transform.position, holoPositionNodes[nodeCounter - 1].Position) >= tolerance)
             {
-                Vector3 direction = Vector3.Normalize(holoNodes[nodeCounter - 1].Position - PlayerManager.Instance.transform.position);
-                float t = -(Vector3.Distance(PlayerManager.Instance.transform.position, holoNodes[0].Position) / Vector3.Distance(holoNodes[holoNodes.Count - 1].Position, holoNodes[0].Position)) + 1;
+                Vector3 direction = Vector3.Normalize(holoPositionNodes[nodeCounter - 1].Position - PlayerManager.Instance.transform.position);
+                float t = -(Vector3.Distance(PlayerManager.Instance.transform.position, holoPositionNodes[0].Position) / Vector3.Distance(holoPositionNodes[holoPositionNodes.Count - 1].Position, holoPositionNodes[0].Position)) + 1;
 
                 PlayerManager.Instance.transform.position = PlayerManager.Instance.transform.position + direction * rewindSpeed;
-                PlayerManager.Instance.transform.rotation = Quaternion.Lerp(holoNodes[holoNodes.Count - 1].Rotation, holoNodes[0].Rotation, t);
+                PlayerManager.Instance.transform.rotation = Quaternion.Lerp(holoPositionNodes[holoPositionNodes.Count - 1].Rotation, holoPositionNodes[0].Rotation, t);
                 yield return new WaitForSeconds(Time.deltaTime);
             }
             nodeCounter--;
@@ -92,31 +94,100 @@ public class RecordPlayback : MonoBehaviour
 
         PlayerManager.Instance.GetComponent<CharacterController>().enabled = true;
         PlayerManager.Instance.GetComponentInChildren<PlayerCamera>().enabled = true;
+        PlayerManager.Instance.GetComponent<PlayerMovement>().enabled = true;
 
-        rewinding = false;
-        yield break;
+        StartCoroutine(SpawnHolo());
     }
 
-    //System.Collections.IEnumerator Playback()
-    //{
-    //    Instantiate(holo);
-    //}
+    System.Collections.IEnumerator SpawnHolo()
+    {
+        UnityEngine.Debug.Log("Spawning Holo");
+        recordPhase = RecordPhase.SpawningHolo;
+
+
+        holoInstance = Instantiate(holoPrefab);
+        holoInstance.transform.position = holoPositionNodes[0].Position;
+
+        yield return new WaitForSeconds(1.0f);
+
+        StartCoroutine(Playback());
+    }
+
+    System.Collections.IEnumerator Playback()
+    {
+        UnityEngine.Debug.Log("Playing back");
+        recordPhase = RecordPhase.PlayingBack;
+
+
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        int interactNodesCounter = 0;
+
+        for (int i = 0; i < holoPositionNodes.Count - 1; i++)
+        {
+            if (interactNodesCounter < holoInteractNodes.Count)
+            {
+                if (stopwatch.ElapsedMilliseconds >= holoInteractNodes[interactNodesCounter].Time)
+                {
+                    //Interaction happened
+                    UnityEngine.Debug.Log($"Interact_{interactNodesCounter}");
+                    interactNodesCounter++;
+                }
+            }
+
+            while (stopwatch.ElapsedMilliseconds < holoPositionNodes[i].Time)
+            {
+                Vector3 distance = holoPositionNodes[i + 1].Position - holoPositionNodes[i].Position;
+
+                holoInstance.transform.position += distance * Time.deltaTime * nodeSpawnRate;
+                holoInstance.transform.rotation = Quaternion.Lerp(holoPositionNodes[i + 1].Rotation, holoPositionNodes[i].Rotation,
+                    Time.time / holoPositionNodes[i].Time);
+                yield return new WaitForFixedUpdate();
+            }
+        }
+        stopwatch.Stop();
+
+        StartCoroutine(KillHolo());
+    }
+
+    System.Collections.IEnumerator KillHolo()
+    {
+        UnityEngine.Debug.Log("Killing Holo");
+        recordPhase = RecordPhase.KillingHolo;
+
+
+        yield return new WaitForSeconds(1.0f);
+        Destroy(holoInstance);
+
+        recordPhase = RecordPhase.None;
+    }
+
+
 
     /// <summary>
     /// Debug to visualize holo nodes
     /// </summary>
     private void OnDrawGizmos()
     {
-        if (holoNodes != null)
+        if (holoPositionNodes != null)
         {
-            for (int i = 0; i < holoNodes.Count; i++)
+            for (int i = 0; i < holoPositionNodes.Count; i++)
             {
                 Gizmos.color = Color.red;
-                Gizmos.DrawSphere(holoNodes[i].Position, 0.25f);
+                Gizmos.DrawSphere(holoPositionNodes[i].Position, 0.25f);
 
-                if (i < holoNodes.Count - 1)
-                    Gizmos.DrawLine(holoNodes[i].Position, holoNodes[i].Position + holoNodes[i].Rotation * Vector3.forward);
+                if (i < holoPositionNodes.Count - 1)
+                    Gizmos.DrawLine(holoPositionNodes[i].Position, holoPositionNodes[i].Position + holoPositionNodes[i].Rotation * Vector3.forward);
             }
+
+            for (int i = 0; i < holoInteractNodes.Count; i++)
+            {
+                Gizmos.color = Color.green;
+                Gizmos.DrawSphere(holoInteractNodes[i].Position, 0.20f);
+            }
+
         }
     }
+
 }
