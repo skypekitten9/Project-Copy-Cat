55c125d989a87c2e52612eb660c18684dad0845a
GustavJernryd <31235205+GustavJernryd@users.noreply.github.com>
2020-09-20 14:50:26 +0200

    Simple button interaction

--- Assets/Scripts/InteractableScripts/ButtonScript.cs
+++ Assets/Scripts/InteractableScripts/ButtonScript.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class ButtonScript : MonoBehaviour
 {
+   
+    [SerializeField]
+    private GameObject rayOrigin;
+    private Ray ray;
+    private int id = 1;
+
     void Start()
     {
         
@@ -11,12 +17,27 @@ public class ButtonScript : MonoBehaviour
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            float range = 5f;
+            ray = new Ray(transform.position, transform.up);
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.distance < range)
+                {
+                    SignalChannel();
+                }
+            }
     }
+}
 
     //Denna metoden är tänkt man ska kalla på när spelaren klickar på knappen. Den meddelar senare vidare till LevelManagern att kanalen ska uppdateras så dörren öppnas.
     private void SignalChannel()
     {
-
+        TestLevelManager.Instance.interactablesArray[id] = !TestLevelManager.Instance.interactablesArray[id];
+        TestLevelManager.Instance.UpdateChannels();
     }
+
 }
