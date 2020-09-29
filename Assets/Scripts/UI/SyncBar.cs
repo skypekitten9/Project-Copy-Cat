8fde79bb46e86b3cfc582d60c4d22850cab9d922
skypekitten9 <victor.enhorning@gmail.com>
2020-09-29 17:36:03 +0200

    Added a way to progress the sync bar and add interaction. However a bug still remains where it follows the bar.

---
+++ Assets/Scripts/UI/SyncBar.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SyncBar : MonoBehaviour
+{
+    private Slider slider;
+    public GameObject interact;
+    private Transform progressionTransform;
+    private List<GameObject> interactionsList;
+    public void SetValue(float value)
+    {
+        slider.value = value;
+    }
+
+    public void SetMaxValue(float value)
+    {
+        slider.maxValue = value;
+    }
+
+    public void SpawnAction(Action action)
+    {
+        switch (action)
+        {
+            case Action.Interact:
+                GameObject interaction = Instantiate(interact, progressionTransform);
+                break;
+
+            case Action.None:
+                Debug.Log("Spawn syncbar NONE");
+                break;
+
+            default:
+                Debug.Log("Spawn syncbar DEFAULT");
+                break;
+        }
+    }
+
+    public void DeleteActions()
+    {
+
+    }
+
+    public void Hide()
+    {
+      
+    }
+
+    public void Show()
+    {
+
+    }
+
+    private void Awake()
+    {
+        slider = gameObject.GetComponent<Slider>();
+        progressionTransform = GameObject.Find("SpawnActionsPosition").GetComponent<Transform>();
+    }
+
+    public void Update()
+    {
+        slider.value += Time.deltaTime;
+        if(Input.GetKeyDown("space"))
+        {
+            Debug.Log("Space pressed in syncbar");
+            SpawnAction(Action.Interact);
+        }
+    }
+}
