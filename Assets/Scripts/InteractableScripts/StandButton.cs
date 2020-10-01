ee3124f5cfcc6df3527bbe7faf33f81e7e251860
GustavJernryd <31235205+GustavJernryd@users.noreply.github.com>
2020-10-01 17:28:49 +0200

    StandButton is now working with limited time

---
+++ Assets/Scripts/InteractableScripts/StandButton.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StandButton : MonoBehaviour
+{
+
+    [SerializeField] private int id;
+    [SerializeField] private float timer;
+    [SerializeField] private float cooldown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timer > 0)
+        {
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                SignalChannel();
+                
+            }
+        }
+        if (cooldown > 0)
+        {
+            cooldown -= Time.deltaTime;
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<PlayerMovement>() != null)
+        {
+            if(cooldown <= 0)
+            {
+                SignalChannel();
+                cooldown = 3f;
+                timer = 3f;
+            }
+            
+        }
+    }
+    private void SignalChannel()
+    {
+        TestLevelManager.Instance.interactablesArray[id] = !TestLevelManager.Instance.interactablesArray[id];
+        TestLevelManager.Instance.UpdateChannels();
+    }
+}
