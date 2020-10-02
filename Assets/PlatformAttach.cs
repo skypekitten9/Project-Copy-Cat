f27105057482a532179850ac47f942721d53f0e5
Filip Moreau <filip.moreau@hotmail.com>
2020-10-02 11:13:50 +0200

    Different object can now attach to the platfrom. Layer collisions detemine which

--- Assets/PlatformAttach.cs
+++ Assets/PlatformAttach.cs
@@ -5,18 +5,13 @@ public class PlatformAttach : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("On platform");
-        if (other.gameObject == PlayerManager.Instance.gameObject)
-        {
-            PlayerManager.Instance.transform.parent = transform.GetChild(0);
-        }
+        other.transform.parent = transform.GetChild(0);
     }
 
     private void OnTriggerExit(Collider other)
     {
         Debug.Log("Off platform");
-        if (other.gameObject == PlayerManager.Instance.gameObject)
-        {
-            PlayerManager.Instance.transform.parent = null;
-        }
+        other.transform.parent = null;
+
     }
 }
