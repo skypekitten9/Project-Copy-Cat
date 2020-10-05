49005bddf3f9cdb6adfc4eb3e76f5a06bdea3824
Filip Moreau <filip.moreau@hotmail.com>
2020-10-06 01:55:15 +0200

    Level Editor scene: Tile selection, camera rotation, tile shader

---
+++ Assets/CameraOrientation.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class CameraOrientation : MonoBehaviour
+{
+    private float sensitivity = 100.0f;
+
+    private float mouseX;
+    private float mouseY;
+
+
+    private void Start()
+    {
+        mouseX = transform.eulerAngles.y;
+        mouseY = -transform.eulerAngles.x;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(2))
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+
+            mouseX += Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+            mouseY += Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+            transform.rotation = Quaternion.Euler(-mouseY, mouseX, 0);
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/LevelEditor/Tile_Selectable.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Tile_Selectable : Selectable
+{
+    [SerializeField] private Material deselected;
+    [SerializeField] private Material selected;
+
+    public override void ToggleSelect()
+    {
+        if (base.isSelected)
+        {
+            //Debug.Log("Deselected: " + gameObject.name);
+            GetComponent<MeshRenderer>().material = deselected;
+            base.isSelected = false;
+        }
+        else
+        {
+            //Debug.Log("Selected: " + gameObject.name);
+            GetComponent<MeshRenderer>().material = selected;
+            base.isSelected = true;
+        }
+
+    }
+}
+
