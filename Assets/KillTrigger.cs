1bcdcde8fe66f42cc9e37dbaaadc3ab406d7fae3
Filip Moreau <filip.moreau@hotmail.com>
2020-10-07 18:16:35 +0200

    Killtrigger which reloads the scene when in contact with the player

---
+++ Assets/KillTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KillTrigger : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        TestLevelManager.Instance.GetComponent<SceneTransition>().ChangeToScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+}
