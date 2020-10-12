d82aef7480d08cfc9e88708bf7ccac051d52b90b
Filip Moreau <filip.moreau@hotmail.com>
2020-10-12 12:24:21 +0200

    Added placeholder UI for level items (to drag & drop from).

---
+++ Assets/Scripts/LevelEditor/LevelItem.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "LevelItem", menuName = "ScriptableObjects/LevelItem")]
+public class LevelItem : ScriptableObject
+{
+    [SerializeField] private Sprite icon;
+    [SerializeField] private GameObject prefab;
+
+
+}
