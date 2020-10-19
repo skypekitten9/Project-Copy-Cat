d6293b076cec93620cddbbb94fa2fbe695a96633
skypekitten9 <victor.enhorning@gmail.com>
2020-10-19 17:04:11 +0200

    Logs keys and prints them to debug.log. Added highlihgting script but does not change the colors yet.

---
+++ Assets/Scripts/UI/HighlightKey.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class HighlightKey : MonoBehaviour
+{
+    public KeyCode key;
+    Color32 defaultColor, highlightColor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        defaultColor = gameObject.GetComponent<TextMeshProUGUI>().color;
+        highlightColor = new Color32(255, 128, 0, 255);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetKeyDown(key))
+        {
+            gameObject.GetComponent<TextMeshProUGUI>().color = highlightColor;
+        }
+        else
+        {
+            gameObject.GetComponent<TextMeshProUGUI>().color = defaultColor;
+        }
+    }
+}
--- Assets/Scripts/UI/KeyLogger.cs
+++ Assets/Scripts/UI/KeyLogger.cs
@@ -1,24 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
 
 public class KeyLogger : MonoBehaviour
 {
-    public Color useColor;
-    Color defaultColor;
+    List<string> keysLogged;
     void Start()
     {
-        
-    }
-
-    void GetKeysToLog()
-    {
-
+        keysLogged = new List<string>();
     }
 
     // Update is called once per frame
-    void Update()
+    void OnGUI()
     {
-        
+        if (Event.current.isKey && Event.current.type == EventType.KeyDown && Event.current.keyCode != KeyCode.None)
+        {
+            Debug.Log(Event.current.keyCode);
+        }
+    }
+
+    void OnApplicationQuit()
+    {
+        //System.IO.File.WriteAllLines(@"C:\Users\Victor\Desktop\Git Projects\Project-Copy-Cat\Assets\KeyLogs", keysLogged);
     }
 }
