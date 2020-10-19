e0305f6b5699273abd6114c01502db08f816df3a
skypekitten9 <victor.enhorning@gmail.com>
2020-10-19 16:01:40 +0200

    Added a script to control highlights and reworked the naming of the keys.

---
+++ Assets/Scripts/UI/KeyLogger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyLogger : MonoBehaviour
+{
+    public Color useColor;
+    Color defaultColor;
+    void Start()
+    {
+        
+    }
+
+    void GetKeysToLog()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
