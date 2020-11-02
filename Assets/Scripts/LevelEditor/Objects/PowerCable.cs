98d5f06acf5835052ec182c511ec9e43d022a061
Filip Moreau <filip.moreau@hotmail.com>
2020-11-02 03:44:08 +0100

    Began working on power cables

---
+++ Assets/Scripts/LevelEditor/Objects/PowerCable.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class PowerCable : MonoBehaviour
+{
+    [SerializeField] private Mesh[] cableModels;
+
+    private MeshFilter meshFilter;
+
+
+    private void OnEnable()
+    {
+        meshFilter = GetComponent<MeshFilter>();
+    }
+
+    void Update()
+    {
+        meshFilter.mesh = cableModels[5];
+    }
+}
