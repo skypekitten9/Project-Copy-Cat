9d6be98117d97c0160323f0d94e6bef9d5221035
skypekitten9 <victor.enhorning@gmail.com>
2020-11-03 14:37:24 +0100

    Implemented patroling with euler rotation.

--- Assets/Scripts/TurretBehavior.cs
+++ Assets/Scripts/TurretBehavior.cs
@@ -3,16 +3,20 @@ using System.Collections.Generic;
 using System.Security.Cryptography;
 using UnityEngine;
 
-enum TurretState {Disabled, Idle, Patroling, Targeting, Fireing}
+enum TurretState {Disabled, Patroling, Targeting, Fireing}
 
 public class TurretBehavior : MonoBehaviour
 {
     GameObject head, eye;
     Animator animator;
-    TurretState state;
+    TurretState state, nextState;
     Vector3 distanceToTarget;
-    public float fireRange, targetRange, idleRange, viewAngle;
+    public float fireRange, targetRange, patrolRange, viewAngle;
     public float chargeTime;
+    public float patrolSpeed, targetSpeed;
+    bool transition, animationPlaying;
+    float animationTimer;
+    float amountTurned;
 
 
 
@@ -22,7 +26,13 @@ public class TurretBehavior : MonoBehaviour
         head = GameObject.Find("Turret_Head");
         eye = GameObject.Find("Turret_Eye");
         state = TurretState.Disabled;
+        nextState = TurretState.Disabled;
         distanceToTarget = Vector3.positiveInfinity;
+        transition = false;
+        animationPlaying = false;
+        targetSpeed = targetSpeed * 10;
+        patrolSpeed = patrolSpeed * 10;
+        amountTurned = 0;
     }
 
     // Update is called once per frame
@@ -30,24 +40,21 @@ public class TurretBehavior : MonoBehaviour
     {
         if (PlayerManager.Instance == null)
             return;
-
         distanceToTarget = PlayerManager.Instance.transform.position - eye.transform.position;
         if (GameManager.Instance.GetComponent<RecordManager>().HoloInstance != null)
             distanceToTarget = GameManager.Instance.GetComponent<RecordManager>().HoloInstance.transform.position - eye.transform.position;
 
         if (Input.GetKeyDown(KeyCode.H))
         {
-            animator.enabled = false;
+            animator.SetBool("isIdle", true);
+            animator.Play("Idle");
         }
 
-        DetermineState();
+        if (animationPlaying) animationTimer -= Time.deltaTime;
         switch (state)
         {
             case TurretState.Disabled:
                 Disabled();
-                break;
-            case TurretState.Idle:
-                Idle();
                 break;
             case TurretState.Patroling:
                 Patroling();
@@ -61,40 +68,104 @@ public class TurretBehavior : MonoBehaviour
             default:
                 break;
         }
-
-    }
-
-    void DetermineState()
-    {
-        if (distanceToTarget.magnitude <= fireRange) state = TurretState.Fireing;
-        else if (distanceToTarget.magnitude <= targetRange) state = TurretState.Targeting;
-        else if (distanceToTarget.magnitude <= idleRange) state = TurretState.Idle;
-        else state = TurretState.Disabled;
         Debug.Log(state);
+        Debug.Log(head.transform.rotation.eulerAngles.y);
     }
 
     void Disabled()
     {
-
-    }
-
-    void Idle()
-    {
-
+        if (transition && animationPlaying && animationTimer <= 0)
+        {
+            animator.enabled = false;
+            animationPlaying = false;
+            transition = false;
+            state = TurretState.Patroling;
+            nextState = TurretState.Patroling;
+            return;
+        }
+        if(transition && !animationPlaying)
+        {
+            animator.enabled = true;
+            animator.Play("Awake");
+            animationPlaying = true;
+            animationTimer = animator.GetCurrentAnimatorStateInfo(0).length;
+        }
+        if (distanceToTarget.magnitude <= patrolRange && !transition)
+        {
+            transition = true;
+        }
     }
 
     void Patroling()
     {
+        if(transition)
+        {
 
+        }
+        switch (nextState)
+        {
+            case TurretState.Disabled:
+                if (animationPlaying && animationTimer <= 0)
+                {
+                    animator.enabled = false;
+                    animationPlaying = false;
+                    transition = false;
+                    state = TurretState.Disabled;
+                    return;
+                }
+                if (!animationPlaying)
+                {
+                    animator.enabled = true;
+                    animator.Play("Sleep");
+                    animationPlaying = true;
+                    animationTimer = animator.GetCurrentAnimatorStateInfo(0).length;
+                }
+                break;
+            case TurretState.Patroling:
+                amountTurned += patrolSpeed * Time.deltaTime;
+                head.transform.Rotate(0, patrolSpeed * Time.deltaTime, 0);
+                if (amountTurned >= viewAngle / 2  || amountTurned <= viewAngle/2 *-1) patrolSpeed *= -1;
+
+                if (distanceToTarget.magnitude <= targetRange)
+                {
+                    nextState = TurretState.Targeting;
+
+                }
+                else if (distanceToTarget.magnitude > patrolRange)
+                {
+                    nextState = TurretState.Disabled;
+                    transition = true;
+                }
+                break;
+            case TurretState.Targeting:
+                state = TurretState.Targeting;
+                break;
+            default:
+                break;
+        }
     }
 
     void Targeting()
     {
+        if (distanceToTarget.magnitude <= fireRange)
+        {
+            state = TurretState.Fireing;
+            nextState = TurretState.Fireing;
 
+        }
+        else if (distanceToTarget.magnitude > targetRange)
+        {
+            state = TurretState.Patroling;
+            nextState = TurretState.Patroling;
+        }
     }
 
     void Fireing()
     {
-
+        if (distanceToTarget.magnitude > fireRange)
+        {
+            state = TurretState.Targeting;
+            nextState = TurretState.Targeting;
+        }
     }
 }
