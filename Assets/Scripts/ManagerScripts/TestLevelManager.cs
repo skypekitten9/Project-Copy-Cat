ab3905e248c6e3bf85f376a2ec27a0f282c16a71
GustavJernryd <31235205+GustavJernryd@users.noreply.github.com>
2020-11-12 17:44:17 +0100

    Revert "Start of level change rework with simple spawn and goalscript"
    This reverts commit 754723bf1ba3eb7e2e3a0a4504905fe2f7124cb6.

--- Assets/Scripts/ManagerScripts/TestLevelManager.cs
+++ Assets/Scripts/ManagerScripts/TestLevelManager.cs
@@ -7,14 +7,29 @@ public class TestLevelManager : MonoBehaviour
     private static TestLevelManager instance = null;
     public static TestLevelManager Instance { get { return instance; } }
 
-   
+    //[SerializeField] private int sceneStartOverride = -1;
+    //[SerializeField] private bool changeScenesOnStart = true;
+
     //Listan som håller alla bools för om dörrar ska vara stängda eller öppna.
     public bool[] interactablesArray;
     [SerializeField] private List<GameObject> doorList;
     [SerializeField] private List<GameObject> leverList;
 
 
+    //[SerializeField] private Level[] levels;
+    //public Level[] Levels { get { return levels; } }
+
+
     public int testInt;
+
+
+    private void OnEnable()
+    {
+        //if (sceneStartOverride != -1 && sceneStartOverride != SceneManager.GetActiveScene().buildIndex)
+        //{
+        //    GetComponent<SceneTransition>().ChangeToScene(sceneStartOverride);
+        //}
+    }
 
     public void Awake()
     {
