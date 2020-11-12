754723bf1ba3eb7e2e3a0a4504905fe2f7124cb6
GustavJernryd <31235205+GustavJernryd@users.noreply.github.com>
2020-11-12 13:39:19 +0100

    Start of level change rework with simple spawn and goalscript

---
+++ Assets/Spawn.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Instantiate(GameManager.Instance.Player, transform.position, Quaternion.identity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
