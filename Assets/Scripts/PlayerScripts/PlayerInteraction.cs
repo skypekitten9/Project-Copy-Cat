8bee67ee007b0de39c0faf24e8cb0a3c35528cf1
Filip Moreau <filip.moreau@hotmail.com>
2020-11-14 14:53:30 +0100

    Migrating project from URP to HDRP

--- Assets/Scripts/PlayerScripts/PlayerInteraction.cs
+++ Assets/Scripts/PlayerScripts/PlayerInteraction.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
-using UnityEngine.Rendering.Universal.Internal;
+
 
 public class PlayerInteraction : MonoBehaviour
 {
