05d15fe9f7b9288fee536af4c8058e2eb10099c4
GustavJernryd <31235205+GustavJernryd@users.noreply.github.com>
2020-11-16 19:57:57 +0100

    Regenerated levels

--- Assets/Spawn.cs
+++ Assets/Spawn.cs
@@ -1,15 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Spawn : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] private GameObject playerPrefab;
+    [SerializeField]private GameObject playerPrefab;
     // Start is called before the first frame update
+    void Awake()
+    {
+        if(SceneManager.GetActiveScene().name != "LevelEditor")
+        {
+            Instantiate(playerPrefab, transform.position, Quaternion.identity);
+        }
+        
+    }
     void Start()
     {
-        Instantiate(playerPrefab, transform.position, Quaternion.identity);
+        
+        
     }
 
     // Update is called once per frame
