5a1b7bed40b478501a72f31b1b051dbe1ee5bab9
GustavJernryd <31235205+GustavJernryd@users.noreply.github.com>
2020-11-27 17:51:47 +0100

    Settings now saves between sessions

--- Assets/Scripts/UI/SettingsMenu.cs
+++ Assets/Scripts/UI/SettingsMenu.cs
@@ -1,15 +1,35 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
+    [SerializeField] Slider volumeSlider;
+    [SerializeField] Slider sensitivitySlider;
+    [SerializeField] Slider fullscreenToggle;
+    [SerializeField] TMP_Text fullscreenText;
     // Start is called before the first frame update
     void Start()
     {
-        
+        audioMixer.SetFloat("Volume", PlayerPrefs.GetFloat("Volume"));
+        volumeSlider.value = PlayerPrefs.GetFloat("Volume");
+        sensitivitySlider.value = PlayerPrefs.GetFloat("Sensitivity");
+        fullscreenToggle.value = PlayerPrefs.GetFloat("Fullscreen");
+        if (fullscreenToggle.value == 1)
+        {
+            Screen.fullScreen = true;
+            fullscreenText.text = "ON";
+        }
+        else if (fullscreenToggle.value == 0)
+        {
+            Screen.fullScreen = false;
+            fullscreenText.text = "OFF";
+        }
     }
 
     // Update is called once per frame
@@ -21,15 +41,28 @@ public class SettingsMenu : MonoBehaviour
     public void SetVolume(float volume)
     {
         audioMixer.SetFloat("Volume", volume);
+        PlayerPrefs.SetFloat("Volume", volume);
     }
 
     public void SetSensitivity(float sensitivity)
     {
         PlayerCamera.mouseSensitivity = sensitivity;
+        PlayerPrefs.SetFloat("Sensitivity", sensitivity);
     }
-    public void SetFullScreen(bool fullScreen)
+    public void SetFullScreen(float fullscreen)
     {
-        Screen.fullScreen = fullScreen;
+        if(fullscreen == 1)
+        {
+            Screen.fullScreen = true;
+            fullscreenText.text = "ON";
+        }
+        else if(fullscreen == 0)
+        {
+            Screen.fullScreen = false;
+            fullscreenText.text = "OFF";
+        }
+
+        PlayerPrefs.SetFloat("Fullscreen", fullscreen);
     }
 
 }
