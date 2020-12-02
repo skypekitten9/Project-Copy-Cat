384101fc252484066dba9d62f83caa47d827c9d0
Carl-Magnus <carl.magnus@hotmail.com>
2020-12-02 20:36:22 +0100

    Added telescope functionality. And some errors regarding the injury screen.

---
+++ Assets/Scripts/InteractableScripts/MineRevealerScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineRevealerScript : MonoBehaviour
+{
+    Camera camera;
+
+    void Start()
+    {
+        camera = gameObject.GetComponentInChildren<Camera>();
+        camera.enabled = false;
+    }
+
+    public void ActivateTelescope(Camera disableCamera)
+    {
+        camera.enabled = true;
+        disableCamera.enabled = false;
+    }
+}
---
+++ Assets/Scripts/InteractableScripts/TelescopeCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TelescopeCamera : MonoBehaviour
+{
+    [SerializeField] public static float mouseSensitivity = 100f;
+
+    private Transform body;
+
+    public float MouseY { get; set; } = 0;
+
+    void Start()
+    {
+        body = transform.parent.gameObject.transform;
+
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        MouseY += Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        MouseY = Mathf.Clamp(MouseY, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(-MouseY, 0f, 0f);
+
+        body.Rotate(Vector3.up * mouseX);
+    }
+}
