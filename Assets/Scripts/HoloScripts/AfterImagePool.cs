26339aa55f4097db3808d76515c3894be63c629f
PatrickCx <patrickglooglass@gmail.com>
2020-12-02 21:22:58 +0100

    character + afterimage polish

--- Assets/Scripts/HoloScripts/AfterImagePool.cs
+++ Assets/Scripts/HoloScripts/AfterImagePool.cs
@@ -2,8 +2,8 @@
 
 class AfterImagePool : MonoBehaviour
 {
-    public float AfterImageFadeTime { get; } = 2000.0f;    //ms
-    private float afterImageSpawnRate = 10.0f;    // times per second
+    public float AfterImageFadeTime { get; } = 600.0f;    //ms
+    private float afterImageSpawnRate = 20.0f;    // times per second
 
     private float afterImagesTimer = 0;
     private int afterImagesIndex = 0;
