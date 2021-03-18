e8c73db09397c4ad558d1e59b2577402a1ca9d82
skypekitten9 <victor.enhorning@gmail.com>
2021-03-18 14:18:56 +0100

    Pause menu is now activated with tab.

--- Assets/Scripts/MenuScripts/PauseMenu.cs
+++ Assets/Scripts/MenuScripts/PauseMenu.cs
@@ -24,7 +24,7 @@ public class PauseMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Tab))
         {
             if (GameIsPaused)
             {
